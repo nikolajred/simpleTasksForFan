4f145615fb4492c9da3949c86d0c2a09467465ca
nikolajred <nikolajred@gmail.com>
2020-11-16 23:55:49 +0200

    first commit

---
+++ src/main/java/Task1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static int inputNumber;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Пожалуйста введите положительное двухзначное число от 10 до 99 включительно: ");
+        while (scanner.hasNext()){
+            if(scanner.hasNextInt()){
+                inputNumber = scanner.
+
+            }
+        }
+    }
+}
