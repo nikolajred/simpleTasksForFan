9e41c3b0cd403c5be5f333fee6000f76a052315e
nikolajred <nikolajred@gmail.com>
2020-11-17 01:04:08 +0200

    second commit

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -2,14 +2,28 @@ import java.util.Scanner;
 
 public class Task1 {
     public static int inputNumber;
+    public static String resultNumber;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Пожалуйста введите положительное двухзначное число от 10 до 99 включительно: ");
         while (scanner.hasNext()){
             if(scanner.hasNextInt()){
-                inputNumber = scanner.
+                inputNumber = scanner.nextInt();
+                if(inputNumber>=10&&inputNumber<=99){
+                    System.out.println("Ваше число равно: " + inputNumber);
+                    break;
+                }else {
+                    System.out.println("Вы ввели некорректное число, повторите ввод");
+                }
 
+            }else{
+                System.out.println("Вы ввели не числовое значение, попробуйте вследующий раз");
+                break;
             }
+
         }
+        resultNumber = (inputNumber/10) + " " +(inputNumber%10);
+        System.out.println("Два одноцифровых числа, разделенных пробелом "+ resultNumber);
+
     }
 }
