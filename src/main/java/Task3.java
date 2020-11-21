2eff56935681f79ad9b29c3b7eb6c2021a22849c
nikolajred <nikolajred@gmail.com>
2020-11-22 00:18:19 +0200

    third commit additional

--- src/main/java/Task3.java
+++ src/main/java/Task3.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
 
 public class Task3 {
-    public static int
+    public static int quantityOfSigns;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Пожайлуста введите целое чесло n которое >=0 или <= 10^9: ");
-        int guesNumber = scanner.nextInt();
-        while (guesNumber>0){
-            guesNumber/=10;
+        long guessNumber = scanner.nextLong();
+        if(guessNumber>=0 || guessNumber <= Math.pow(10, 9)){
+            while (guessNumber>0){
+                guessNumber/=10;
+                quantityOfSigns++;
+
+            }
+            System.out.println("Колличество знаков в вашем чесле равно числу: " + quantityOfSigns);
+
+        }else{
+            System.out.println("Вы ввели некорректное число повторите ввод: ");
         }
+
     }
 }
